0e3095e92df6958b2fcbb7757a8c01eb804ef322
dragonlost <dragonlost1@hotmail.fr>
2025-03-04 23:23:36 +0100

    update for 24.04 python

--- ConfigTinker/install_browsepy.sh
+++ ConfigTinker/install_browsepy.sh
@@ -21,11 +21,13 @@ echo "================================================="
 echo "================================================="
 cd ${dirinstall}
 # s'assurer que que pip est installe
-sudo apt-get -y install python3-pip python3-setuptools
+sudo apt-get -y install python3-pip python3-setuptools 
+sudo apt-get -y install pipx
 # installer browsepy
 pip3 install scandir
 pip3 install backports.shutil_get_terminal_size
-pip3 install git+https://github.com/dragonlost/browsepy.git
+pipx install git+https://github.com/dragonlost/browsepy.git
+pipx ensurepath
 # Lancer le serveur au demarrage
 # placer la commande sous .config/autostart
 #cat ${dirinstall}/browsepy.desktop | sed -e "s#MOI#${HOME}#g" > /tmp/browsepy.desktop
@@ -39,7 +41,7 @@ sudo updatedb     # mettre à jour la base d'indexation
 dir_browsepy="/home/${USER}/.local/bin/browsepy"  #récupérer le chemin du binaire browsepy
 
 sudo rm /tmp/browsepy.service
-echo -e '[Unit]\nDescription=Browsepy\nAfter=multi-user.target\n\n[Service]\nType=idle\nUser='${MOI}'\nExecStart=browsepy --directory /home/'${MOI}' 0.0.0.0 8180\nRestart=always\nRestartSec=5\n[Install]\nWantedBy=multi-user.target' >> /tmp/browsepy.service
+echo -e '[Unit]\nDescription=Browsepy\nAfter=multi-user.target\n\n[Service]\nType=idle\nUser='${MOI}'\nExecStart=/home/'${MOI}'/.local/bin/browsepy --directory /home/'${MOI}' 0.0.0.0 8180\nRestart=always\nRestartSec=5\n[Install]\nWantedBy=multi-user.target' >> /tmp/browsepy.service
 
 sudo rm ${dir}/browsepy.service
 sudo cp /tmp/browsepy.service ${dir}/
